bd1e8751fef3f7be6e898eb4d9bea0857095e09d
Bandapalli Trinadh <thrinnadhh.b97@gmail.com>
2026-04-10 17:02:23 +0530

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 window.__SUPABASE_CONFIG__ = {
   // Replace with your project values from Supabase > Project Settings > API
-  url: "YOUR_SUPABASE_URL",
-  anonKey: "YOUR_SUPABASE_ANON_KEY",
+  url: "https://hjlbtxctytkwrhowutvd.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqbGJ0eGN0eXRrd3Job3d1dHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MDc1MTcsImV4cCI6MjA5MTM4MzUxN30.Mnpbw4GYfX_MzvT11v27tmIAvNePdyumDEi_-FJqYlI",
 };
